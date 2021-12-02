e2cd62e0a6f97ecb31d9fb4c1d58e6ec9545464e
luk-kop <luk.kopicki@gmail.com>
2021-12-02 14:56:51 +0100

    Refactored main.py.
    Details:
    - Added main script func.
    - Added perform_action_on_instance func.
    - Refactore check_tag func.

--- main.py
+++ main.py
@@ -1,22 +1,15 @@
 import sys
-from typing import Union
 
 import boto3
+from botocore.exceptions import ClientError
 
 
-def check_tag(tags: Union[list, None], tag_key: str, tag_value: str = '') -> bool:
+def check_tag_exist(tags: list, tag_key: str, tag_value: str = '') -> bool:
     """
-    Returns whether to perform action on the EC2 instance.
-    Checks that any instance tags are assigned. Then verifies whether a tag with the given key or given key and value
-    pair exists in the list of instance tags.
+    Checks whether a specified tag is in the list of given tags.
     """
-    if not tags and not tag_key:
-        # blad bo jezeli ma byc tylko usunieta instancja z konkretnym tagiem to sie wykona!!!
-        return True
     for tag in tags:
-        if not tag_key and not tag_value:
-            return True
-        elif tag_value:
+        if tag_value:
             if tag.get('Key') == tag_key and tag.get('Value') == tag_value:
                 return True
         else:
@@ -25,7 +18,7 @@ def check_tag(tags: Union[list, None], tag_key: str, tag_value: str = '') -> boo
     return False
 
 
-def check_proper_ec2_state(action: str, ec2_current_state: str) -> bool:
+def check_proper_instance_state(action: str, ec2_current_state: str) -> bool:
     """
     Checks whether the EC2 instance is in desired state.
     """
@@ -46,32 +39,84 @@ def is_action_allowed(action: str) -> None:
         sys.exit()
 
 
-if __name__ == '__main__':
-    aws_region_name = 'eu-west-1'
-    # Action that should be performed on the EC2 instances
-    ec2_action = 'terminate'
+def perform_action_on_instance(action: str, instance, **kwargs) -> bool:
+    """
+    Performs the specified action (stop or terminate) on the EC2 instance if necessary.
+    Returns True if the action was performed on the EC2 instance.
+    """
+    instance_tags = instance.tags
+    take_action = False
+    # Get additional attrs
+    no_tags: bool = kwargs.get('ec2_no_tags', False)
+    no_name_tag: bool = kwargs.get('ec2_no_name_tag', False)
+    specified_tag: dict = kwargs.get('ec2_tag', {})
+    if no_tags and not instance_tags:
+        # Perform an action if EC2 instance has NO tag assigned
+        take_action = True
+    elif no_name_tag:
+        # Perform an action ONLY if tag "Name" IS NOT assigned to EC2 instance
+        if instance_tags:
+            take_action = not check_tag_exist(tags=instance_tags, tag_key='Name')
+        else:
+            take_action = True
+    elif specified_tag and instance_tags:
+        # Perform an action ONLY if specified tag IS assigned to EC2 instance
+        take_action = check_tag_exist(tags=instance_tags, **specified_tag)
+    if not take_action:
+        return take_action
+    # Take action on EC2 instance if necessary
+    if action == 'terminate':
+        instance.terminate()
+        print(f'Instance with id "{instance.id}" terminated...')
+    else:
+        try:
+            instance.stop()
+            print(f'Instance with id "{instance.id}" stopped...')
+        except ClientError as err:
+            # Handle exception when instance in 'pending' state.
+            error_msg = err.response['Error']['Message']
+            print(f'Error: {error_msg}. Try later...')
+    return take_action
 
-    # Tag to check - default not tags (action on instances without tags)
-    ec2_tag = {
-        'tag_key': 'Environment',
-        'tag_value': 'Test',
-    }
 
+def main(aws_region, ec2_action, **kwargs) -> None:
+    """
+    Script's main func.
+    """
+    is_action_allowed(action=ec2_action)
+    # Get additional func args
+    additional_args = kwargs
     # Get all EC2 instances in specified AWS region
-    ec2 = boto3.resource('ec2', region_name=aws_region_name)
+    ec2 = boto3.resource('ec2', region_name=aws_region)
     ec2_instances_all = ec2.instances.all()
 
     for instance in ec2_instances_all:
-        # Verify instance tag
-        instance_tag_status: bool = check_tag(tags=instance.tags, **ec2_tag)
+        instance_current_state = instance.state['Name']
+        instance_proper_state: bool = check_proper_instance_state(action=ec2_action, ec2_current_state=instance_current_state)
+        print(f'{instance.id}, {instance_current_state}, instance_tags: {instance.tags}')   # For tests
+        # Perform action on EC2 instance if it is in the correct state
+        if instance_proper_state:
+            action_taken: bool = perform_action_on_instance(action=ec2_action, instance=instance, **additional_args)
+    if 'action_taken' not in locals() or not action_taken:
+        print('Nothing to do...')
 
-        instance_state = instance.state['Name']
-        print(f'{instance.id}, {instance_state}, instance_tags: {instance.tags}, tag_status: {instance_tag_status}')
 
-        if instance_tag_status:
-            if ec2_action == 'terminate' and instance_state not in ['terminated', 'shutting-down']:
-                instance.terminate()
-                print(f'Instance with id "{instance.id}" terminated...')
-            elif ec2_action == 'stop' and instance_state not in ['terminated', 'shutting-down', 'stopping', 'stopped']:
-                instance.stop()
-                print(f'Instance with id "{instance.id}" stopped...')
+if __name__ == '__main__':
+    aws_region_name = 'eu-west-1'
+    # Action that should be performed on the EC2 instances
+    ec2_action = 'stop'
+
+    main_attrs = {
+        'aws_region': aws_region_name,
+        'ec2_action': ec2_action
+    }
+
+    main_attrs['ec2_no_tags'] = True
+    # main_attrs['ec2_no_name_tag'] = True
+    # main_attrs['ec2_tag'] = {
+    #     'tag_key': 'Name',
+    #     'tag_value': 'test1'
+    # }
+    # Run script's main func
+    main(**main_attrs)
+
